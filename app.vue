3dabc5633b98a27c9877a23d3e5ffe08c9348df3
takanori <takanori@goopter.com>
2023-03-07 22:06:23 -0800

    added tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    Awesome Portfolio here!!
+    <!-- navbar -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font-0mono" href="">Navbar</a>
+    </header>
   </div>
 </template>
