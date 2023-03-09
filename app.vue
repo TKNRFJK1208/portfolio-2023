ae14783718598c525de79952948b0e7add9b3dd9
takanori <takanori@goopter.com>
2023-03-08 22:38:19 -0800

    added components and pages

--- app.vue
+++ app.vue
@@ -4,5 +4,6 @@
     <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
       <a class="text-3xl font-0mono" href="">Navbar</a>
     </header>
+    <NuxtPage />
   </div>
 </template>
